1e4af6ce79def951e76f489cb2a70a352a9f6153
ricardolugo39 <66835344+ricardolugo39@users.noreply.github.com>
2024-02-22 12:16:21 -0500

    Initial commit

---
+++ logic/static/logic/routes/email_interest.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
+from logic import bp
+
+email_interest = Blueprint('email_interest', __name__)
+
+@bp.route('/email_interest', methods=['GET', 'POST'])
+def display_order_confirmation():
+    return render_template('email_interest.html', template_folder=current_app.template_folder)
