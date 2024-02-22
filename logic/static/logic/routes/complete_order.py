74b521f550c76a25b2deca40487f5339610b642e
ricardolugo39 <66835344+ricardolugo39@users.noreply.github.com>
2024-02-22 13:52:57 -0500

    Your commit message here

--- logic/static/logic/routes/complete_order.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, current_app, Blueprint, request, redirect
-
-complete_order_bp = Blueprint('complete_order', __name__)
-
-@complete_order_bp.route('/complete_order', methods=['GET', 'POST'])
-def display_complete_order():
-    return render_template ('complete_order.html', template_folder=current_app.template_folder)
