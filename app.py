a59280240e3cdb89f52280f3a78c8c4cb291d4ef
ricardolugo39 <66835344+ricardolugo39@users.noreply.github.com>
2024-02-22 16:54:51 -0500

    "updated"

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ app.register_blueprint(free_pair_bp)
 app.register_blueprint(complete_order_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
