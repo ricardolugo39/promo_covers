eedc6ce77e918ad22158a9fab9ae01a48e1f536c
ricardolugo39 <66835344+ricardolugo39@users.noreply.github.com>
2024-02-27 09:59:39 -0500

    db changes

--- logic/database.py
+++ logic/database.py
@@ -3,43 +3,50 @@ import os
 
 DATABASE_PATH = 'single_database.db'
 
+
 def init_database():
     conn = sqlite3.connect(DATABASE_PATH)
     cursor = conn.cursor()
 
-    # Drop the existing tables
-    cursor.execute('DROP TABLE IF EXISTS user_data')
-    cursor.execute('DROP TABLE IF EXISTS order_data')
+    # Check if the tables already exist
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='user_data'")
+    user_data_table_exists = cursor.fetchone()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='order_data'")
+    order_data_table_exists = cursor.fetchone()
 
     # Create the 'user_data' table if it doesn't exist
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS user_data (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    ''')
+    if not user_data_table_exists:
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS user_data (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
 
     # Create the 'order_data' table if it doesn't exist
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS order_data (
-            id INTEGER PRIMARY KEY,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            order_info TEXT NOT NULL,
-            color TEXT NOT NULL,
-            size TEXT NOT NULL,
-            address TEXT NOT NULL,
-            state TEXT NOT NULL,
-            city TEXT NOT NULL,
-            zip TEXT NOT NULL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    ''')
+    if not order_data_table_exists:
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS order_data (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                order_info TEXT NOT NULL,
+                color TEXT NOT NULL,
+                size TEXT NOT NULL,
+                address TEXT NOT NULL,
+                state TEXT NOT NULL,
+                city TEXT NOT NULL,
+                zip TEXT NOT NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
 
     conn.commit()
     conn.close()
+
 
 def store_user_data(name, email):
     conn = sqlite3.connect(DATABASE_PATH)
